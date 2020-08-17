c0473b9146f60615bd762d23f8e22a9a0863d537
tonythomasm6 <68453981+tonythomasm6@users.noreply.github.com>
2020-08-17 13:26:00 +1000

    FIT5032 Week3 changes push

---
+++ FIT5032_Week03/Exercise/ExampleDictionary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FIT5032_Week03.Exercise
+{
+    public class ExampleDictionary
+    {
+        public void Example()
+        {
+            Dictionary<int, Student> studentDictionary = new Dictionary<int, Student>();
+            Student s1 = new Student("Mathew", "0460318009");
+            Student s2 = new Student("Kim", "0460318010");
+
+            studentDictionary.Add(1, s1);
+            studentDictionary.Add(2, s2);
+
+            Student result = new Student("", "");
+
+            studentDictionary.TryGetValue(1,out result);
+
+            String n = result.Name;
+            String p = result.PhoneNumber;
+        }
+      
+        
+    }
+}
